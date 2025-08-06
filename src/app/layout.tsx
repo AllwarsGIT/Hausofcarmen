1ac1fc3846bd900bb555f749e469f8e1dc4bc8c4
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2025-08-06 13:17:56 +0200

    Minor viewport fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/Components/Header";
@@ -16,6 +16,11 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Haus of Carmen",
   description: "Página de la Haus of Carmen, un lugar para disfrutar de espectáculos drag únicos y memorables.",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
