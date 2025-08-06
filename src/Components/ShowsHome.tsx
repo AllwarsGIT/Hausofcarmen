a62b654033059bb388afca4d1fcb6f3e63361196
DESKTOP-8CPSTED\Álvaro Sánchez Torre <allwarsps@gmail.com>
2025-08-06 13:23:52 +0200

    final fix carousel responsive

--- src/Components/ShowsHome.tsx
+++ src/Components/ShowsHome.tsx
@@ -7,7 +7,7 @@ function ShowsHome() {
 
   
   return (
-    <section className="flex flex-col w-full items-center justify-center h-[60vh] bg-black">
+    <section className="flex flex-col w-full items-center justify-center min-h-[60vh] bg-black">
         <div className="flex flex-row mt-15 w-full justify-center ">
             <h1 className="text-pinkSecondary text-2xl font-bold ">ESPECTÁCULOS</h1>
         </div>
